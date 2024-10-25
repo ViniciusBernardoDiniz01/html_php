7b4a40493b53e87d654da735127f68d5157cbbc0
Vinicius <vinicius.diniz.senai404@gmail.com>
2024-10-25 09:49:01 -0300

    landing pages
    adicionando as pasta da lading pages e trazendo segurança para o codigo

---
+++ Backend/app_help_desk_ladingpag/consultar_chamado.php
@@ -0,0 +1,82 @@
+<?php require_once("login.php");?>
+
+<?php 
+// chama o arquivo e o le
+$arquivo = fopen('../../app_help_desk/registro.txt','r');
+$chamados = [];
+
+// usa um loop e usa o fgets para ler linha por linha ate a ultima
+while(!feof($arquivo)){
+  $registro = fgets($arquivo);
+  // as informaçoes do  registro vão para a array
+  $chamados [] = $registro;
+}
+// fecha o arquivo
+fclose ($arquivo);
+// mostra a array
+// echo '<pre>';
+// print_r ($chamados);
+// echo '</pre>';
+?>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>App Help Desk</title>
+
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="bootstrap.css">
+    </style>
+  </head>
+
+  <body>
+  
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        App Help Desk
+      </a>
+      <div class="botao-sair"><a href="logoff.php"><button>sair</button></a></div>
+    </nav>
+    
+
+    <div class="container">    
+      <div class="row">
+
+        <div class="card-consultar-chamado">
+          <div class="card">
+            <div class="card-header">
+              Consulta de chamado
+            </div>
+            
+            <div class="card-body">
+              <?php 
+              // quebra a linha das array apos a #
+              foreach($chamados as $chamado){
+                $chamados_dados = explode('#',$chamado);
+                // deixa com que a linha que esta vazia não apareça nos cardes
+                if(count($chamados_dados) < 3){
+                  continue;
+                }
+              ?>
+              <div class="card mb-3 bg-light">
+                <div class="card-body">
+                  <!-- coloca dentro das caixinhas titulo categoria e comentario os valores 
+                  da array antes preenchida -->
+                  <h5 class="card-title"><?php echo $chamados_dados[0];?></h5>
+                  <h6 class="card-subtitle mb-2 text-muted"><?php echo $chamados_dados[1];?></h6>
+                  <p class="card-text"><?php echo $chamados_dados[2];?></p>
+                </div>
+              </div>
+              <?php  }?>
+              <div class="row mt-5">
+                <div class="col-6">
+                  <a href="painel.php" class="btn btn-lg btn-warning btn-block" type="submit">Voltar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ Backend/app_help_desk_ladingpag/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Css/style.css">
+    <title>Microsoft</title>
+</head>
+<body>
+    <header>
+    <div class="imagem"><a href="https://www.microsoft.com/pt-br"><img src=".//Imagem/Microsoft_logo.svg.png" alt="Microsoft" height="50" width="50"></a></div>
+    <div class="text">
+    <div class="cor"><h1>Bem-vindo a Microsoft</h1></div>
+    <div class="cor2"><h4>Estamos felizes em te-lo aqui preencha o formulario para contato</h4></div>
+    <center><a href="entrar.php" class="login" type="submit"><button>Login</button></a></center>
+    </div>
+    </header>
+    <div class="body"><div id="img"><img src="./Imagem/imagem mulher.png" alt="" width="750" height="500"></div>
+    <br><br>
+    <div class="form"><center><h3>Fale conosco</h3></center>    
+    <br>
+    <p>Nome:</p>
+    <form action="">
+        <label for="nome"></label>
+        <input type="nome" id="nome" placeholder="Digite seu nome aqui" required>
+    </form>
+    <br><br>
+    <p>Email:</p>
+    <div id="email"><form action="">
+        <label for="email"></label>
+        <input type="email"id="email"placeholder="Digite seu email aqui" required>
+    </form></div>
+    <br><br>
+    <p>Mensagem:</p>
+    <textarea name="mensagem" placeholder="Digite sua mensagem aqui" rows="5" cols="20"></textarea>
+    <br>
+    <button>Enviar</button></div></div>
+    <br>
+    <footer><div class="direitos">©2024 Ds Company. Todos os direitos reservados</div></footer>
+</body>
+</html>
